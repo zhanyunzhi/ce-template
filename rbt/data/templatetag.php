04026b46b21917e416a77e81d4155dfa48534490
zhanyunzhi <540562332@qq.com>
2017-12-08 17:42:18 +0800

    首页基本套好模本

--- rbt/data/templatetag.php
+++ rbt/data/templatetag.php
@@ -14,379 +14,6 @@
       'attr1' => '0',
     ),
   ),
-  3 => 
-  array (
-    'id' => '3',
-    'name' => '首页第一行栏目图片说明二',
-    'tagfrom' => 'category',
-    'tagcontent' => 'null',
-    'setting' => 
-    array (
-      'onlymodify' => '',
-      'catid' => '11',
-      'tagtemplate' => 'tag_category-index-1.html',
-      'submit' => '提交',
-      'attr1' => '0',
-    ),
-  ),
-  4 => 
-  array (
-    'id' => '4',
-    'name' => '首页第一行栏目图片说明三',
-    'tagfrom' => 'category',
-    'tagcontent' => 'null',
-    'setting' => 
-    array (
-      'onlymodify' => '',
-      'catid' => '4',
-      'tagtemplate' => 'tag_category-index-1.html',
-      'submit' => '提交',
-      'attr1' => '0',
-    ),
-  ),
-  7 => 
-  array (
-    'id' => '7',
-    'name' => '首页第四行栏目名称',
-    'tagfrom' => 'category',
-    'tagcontent' => 'null',
-    'setting' => 
-    array (
-      'onlymodify' => '',
-      'catid' => '3',
-      'tagtemplate' => 'tag_category-title.html',
-      'submit' => '提交',
-      'attr1' => '0',
-    ),
-  ),
-  8 => 
-  array (
-    'id' => '8',
-    'name' => '首页第二行推荐图片三张',
-    'tagfrom' => 'content',
-    'tagcontent' => 'null',
-    'setting' => 
-    array (
-      'onlymodify' => '',
-      'catid' => '0',
-      'son' => '1',
-      'typeid' => '0',
-      'spid' => '0',
-      'length' => '20',
-      'introduce_length' => '',
-      'ordertype' => 'adddate-desc',
-      'istop' => '0',
-      'limit' => '3',
-      'thumb' => 'on',
-      'attr1' => '0',
-      'tagtemplate' => 'tag_content-index-2.html',
-      'submit' => '提交',
-    ),
-  ),
-  9 => 
-  array (
-    'id' => '9',
-    'name' => '首页第二行图文三条',
-    'tagfrom' => 'content',
-    'tagcontent' => 'null',
-    'setting' => 
-    array (
-      'onlymodify' => '',
-      'catid' => '2',
-      'son' => '1',
-      'typeid' => '0',
-      'spid' => '0',
-      'province_id' => '0',
-      'city_id' => '0',
-      'section_id' => '0',
-      'length' => '20',
-      'introduce_length' => '',
-      'ordertype' => 'adddate-desc',
-      'istop' => '0',
-      'limit' => '3',
-      'thumb' => 'on',
-      'attr1' => '0',
-      'tagtemplate' => 'tag_content-index-6-pic3-2.html',
-      'submit' => '提交',
-    ),
-  ),
-  10 => 
-  array (
-    'id' => '10',
-    'name' => '首页第三行栏目',
-    'tagfrom' => 'category',
-    'tagcontent' => 'null',
-    'setting' => 
-    array (
-      'onlymodify' => '',
-      'catid' => '1',
-      'tagtemplate' => 'tag_category-categorycontent.html',
-      'submit' => '提交',
-      'attr1' => '0',
-    ),
-  ),
-  12 => 
-  array (
-    'id' => '12',
-    'name' => '首页第二行栏目名称',
-    'tagfrom' => 'category',
-    'tagcontent' => 'null',
-    'setting' => 
-    array (
-      'onlymodify' => '',
-      'catid' => '2',
-      'tagtemplate' => 'tag_category-title.html',
-      'submit' => '提交',
-      'attr1' => '0',
-    ),
-  ),
-  13 => 
-  array (
-    'id' => '13',
-    'name' => '首页第六行图片列表',
-    'tagfrom' => 'category',
-    'tagcontent' => 'null',
-    'setting' => 
-    array (
-      'onlymodify' => '',
-      'catid' => '14',
-      'tagtemplate' => 'tag_category-index-6-pic.html',
-      'submit' => '提交',
-      'attr1' => '0',
-    ),
-  ),
-  14 => 
-  array (
-    'id' => '14',
-    'name' => '页底栏目一',
-    'tagfrom' => 'category',
-    'tagcontent' => 'null',
-    'setting' => 
-    array (
-      'onlymodify' => '',
-      'catid' => '1',
-      'tagtemplate' => 'tag_category-foot-cat-list.html',
-      'submit' => '提交',
-      'attr1' => '0',
-    ),
-  ),
-  15 => 
-  array (
-    'id' => '15',
-    'name' => '页底栏目二',
-    'tagfrom' => 'category',
-    'tagcontent' => 'null',
-    'setting' => 
-    array (
-      'onlymodify' => '',
-      'catid' => '3',
-      'tagtemplate' => 'tag_category-foot-cat-list.html',
-      'submit' => '提交',
-      'attr1' => '0',
-    ),
-  ),
-  16 => 
-  array (
-    'id' => '16',
-    'name' => '页底栏目三',
-    'tagfrom' => 'category',
-    'tagcontent' => 'null',
-    'setting' => 
-    array (
-      'onlymodify' => '',
-      'catid' => '2',
-      'tagtemplate' => 'tag_category-foot-cat-list.html',
-      'submit' => '提交',
-      'attr1' => '0',
-    ),
-  ),
-  17 => 
-  array (
-    'id' => '17',
-    'name' => '内容页底图文产品三条',
-    'tagfrom' => 'content',
-    'tagcontent' => 'null',
-    'setting' => 
-    array (
-      'onlymodify' => '',
-      'catid' => '3',
-      'son' => '1',
-      'typeid' => '0',
-      'spid' => '0',
-      'province_id' => '0',
-      'city_id' => '0',
-      'section_id' => '0',
-      'length' => '20',
-      'introduce_length' => '',
-      'ordertype' => 'adddate-desc',
-      'istop' => '0',
-      'limit' => '3',
-      'thumb' => 'on',
-      'attr1' => '0',
-      'tagtemplate' => 'tag_content-content-bottom.html',
-      'submit' => '提交',
-    ),
-  ),
-  18 => 
-  array (
-    'id' => '18',
-    'name' => '内页右侧栏目名称',
-    'tagfrom' => 'category',
-    'tagcontent' => 'null',
-    'setting' => 
-    array (
-      'onlymodify' => '',
-      'catid' => '2',
-      'tagtemplate' => 'tag_category-index-title.html',
-      'submit' => '提交',
-      'attr1' => '0',
-    ),
-  ),
-  19 => 
-  array (
-    'id' => '19',
-    'name' => '内页右侧内容列表',
-    'tagfrom' => 'content',
-    'tagcontent' => 'null',
-    'setting' => 
-    array (
-      'onlymodify' => '',
-      'catid' => '2',
-      'son' => '1',
-      'typeid' => '0',
-      'spid' => '0',
-      'province_id' => '0',
-      'city_id' => '0',
-      'section_id' => '0',
-      'length' => '20',
-      'introduce_length' => '',
-      'ordertype' => 'adddate-desc',
-      'istop' => '0',
-      'limit' => '10',
-      'attr1' => '0',
-      'tagtemplate' => 'tag_content.html',
-      'submit' => '提交',
-    ),
-  ),
-  20 => 
-  array (
-    'id' => '20',
-    'name' => '首页第四行滚动图片',
-    'tagfrom' => 'content',
-    'tagcontent' => 'null',
-    'setting' => 
-    array (
-      'onlymodify' => '',
-      'catid' => '10',
-      'son' => '1',
-      'typeid' => '0',
-      'spid' => '0',
-      'province_id' => '0',
-      'city_id' => '0',
-      'section_id' => '0',
-      'length' => '12',
-      'introduce_length' => '',
-      'ordertype' => 'adddate-desc',
-      'istop' => '0',
-      'limit' => '10',
-      'attr1' => '0',
-      'tagtemplate' => 'tag_content-gundong.html',
-      'submit' => '提交',
-    ),
-  ),
-  21 => 
-  array (
-    'id' => '21',
-    'name' => '首页第六行栏目名称',
-    'tagfrom' => 'category',
-    'tagcontent' => 'null',
-    'setting' => 
-    array (
-      'onlymodify' => '',
-      'catid' => '14',
-      'tagtemplate' => 'tag_category-title.html',
-      'submit' => '提交',
-      'attr1' => '0',
-    ),
-  ),
-  22 => 
-  array (
-    'id' => '22',
-    'name' => '首页第一行栏目名称',
-    'tagfrom' => 'category',
-    'tagcontent' => 'null',
-    'setting' => 
-    array (
-      'onlymodify' => '',
-      'catid' => '1',
-      'tagtemplate' => 'tag_category-title-index-1.html',
-      'submit' => '提交',
-      'attr1' => '0',
-    ),
-  ),
-  23 => 
-  array (
-    'id' => '23',
-    'name' => '首页第一行栏目图片说明四',
-    'tagfrom' => 'category',
-    'tagcontent' => 'null',
-    'setting' => 
-    array (
-      'onlymodify' => '',
-      'catid' => '7',
-      'tagtemplate' => 'tag_category-index-1.html',
-      'submit' => '提交',
-      'attr1' => '0',
-    ),
-  ),
-  24 => 
-  array (
-    'id' => 24,
-    'name' => '首页第六行图片12张',
-    'tagfrom' => 'content',
-    'tagcontent' => 'null',
-    'setting' => 
-    array (
-      'onlymodify' => '',
-      'catid' => '14',
-      'son' => '1',
-      'typeid' => '0',
-      'spid' => '0',
-      'province_id' => '0',
-      'city_id' => '0',
-      'section_id' => '0',
-      'length' => '',
-      'introduce_length' => '',
-      'ordertype' => 'adddate-desc',
-      'istop' => '0',
-      'limit' => '12',
-      'thumb' => 'on',
-      'attr1' => '0',
-      'tagtemplate' => 'tag_content-index-6.html',
-      'submit' => '提交',
-      'catname' => '',
-      'htmldir' => '',
-      'introduce' => '',
-    ),
-  ),
-  25 => 
-  array (
-    'id' => 25,
-    'name' => '页底栏目四',
-    'tagfrom' => 'category',
-    'tagcontent' => 'null',
-    'setting' => 
-    array (
-      'onlymodify' => '',
-      'catid' => '5',
-      'tagtemplate' => 'tag_category-foot-cat-list.html',
-      'submit' => '提交',
-      'attr1' => '0',
-      'catname' => '',
-      'htmldir' => '',
-      'introduce' => '',
-    ),
-  ),
   26 => 
   array (
     'id' => 26,
@@ -396,47 +23,6 @@
     'setting' => 
     array (
       'onlymodify' => '',
-      'submit' => '提交',
-      'attr1' => '0',
-      'catname' => '',
-      'htmldir' => '',
-      'introduce' => '',
-    ),
-  ),
-  27 => 
-  array (
-    'id' => '27',
-    'name' => '首页产品中心内容',
-    'tagfrom' => 'content',
-    'tagcontent' => 'null',
-    'setting' => 
-    array (
-      'onlymodify' => '',
-      'catid' => '10',
-      'son' => '1',
-      'typeid' => '0',
-      'spid' => '0',
-      'length' => '',
-      'introduce_length' => '',
-      'ordertype' => 'adddate-desc',
-      'istop' => '0',
-      'limit' => '',
-      'attr1' => '0',
-      'tagtemplate' => 'tag_content-rbt.html',
-      'submit' => '提交',
-    ),
-  ),
-  28 => 
-  array (
-    'id' => 28,
-    'name' => '首页产品中心标题',
-    'tagfrom' => 'category',
-    'tagcontent' => 'null',
-    'setting' => 
-    array (
-      'onlymodify' => '',
-      'catid' => '3',
-      'tagtemplate' => 'tag_category-title-rbt.html',
       'submit' => '提交',
       'attr1' => '0',
       'catname' => '',
@@ -464,6 +50,101 @@
       'limit' => '3',
       'attr1' => '0',
       'tagtemplate' => 'tag_content-product-rbt.html',
+      'submit' => '提交',
+    ),
+  ),
+  30 => 
+  array (
+    'id' => 30,
+    'name' => '首页解决方案标题和内容',
+    'tagfrom' => 'content',
+    'tagcontent' => 'null',
+    'setting' => 
+    array (
+      'onlymodify' => '',
+      'catid' => '14',
+      'son' => '0',
+      'typeid' => '0',
+      'spid' => '0',
+      'length' => '',
+      'introduce_length' => '',
+      'ordertype' => 'adddate-desc',
+      'istop' => '0',
+      'limit' => '12',
+      'attr1' => '0',
+      'tagtemplate' => 'tag_content-solution-rbt.html',
+      'submit' => '提交',
+      'catname' => '',
+      'htmldir' => '',
+      'introduce' => '',
+    ),
+  ),
+  31 => 
+  array (
+    'id' => '31',
+    'name' => '首页关于我们',
+    'tagfrom' => 'content',
+    'tagcontent' => 'null',
+    'setting' => 
+    array (
+      'onlymodify' => '',
+      'catid' => '1',
+      'son' => '0',
+      'typeid' => '0',
+      'spid' => '0',
+      'length' => '',
+      'introduce_length' => '',
+      'ordertype' => 'adddate-desc',
+      'istop' => '0',
+      'limit' => '',
+      'attr1' => '0',
+      'tagtemplate' => 'tag_content-about-rbt.html',
+      'submit' => '提交',
+    ),
+  ),
+  32 => 
+  array (
+    'id' => '32',
+    'name' => '首页联系我们',
+    'tagfrom' => 'content',
+    'tagcontent' => 'null',
+    'setting' => 
+    array (
+      'onlymodify' => '',
+      'catid' => '5',
+      'son' => '0',
+      'typeid' => '0',
+      'spid' => '0',
+      'length' => '',
+      'introduce_length' => '',
+      'ordertype' => 'adddate-desc',
+      'istop' => '0',
+      'limit' => '',
+      'attr1' => '0',
+      'tagtemplate' => 'tag_content-contact-rbt.html',
+      'submit' => '提交',
+    ),
+  ),
+  33 => 
+  array (
+    'id' => '33',
+    'name' => '首页我们团队',
+    'tagfrom' => 'content',
+    'tagcontent' => 'null',
+    'setting' => 
+    array (
+      'onlymodify' => '',
+      'catid' => '16',
+      'son' => '0',
+      'typeid' => '0',
+      'spid' => '0',
+      'length' => '',
+      'introduce_length' => '',
+      'ordertype' => 'adddate-desc',
+      'istop' => '0',
+      'limit' => '4',
+      'attr1' => '0',
+      'tagtemplate' => 'tag_content-team-rbt.html',
       'submit' => '提交',
     ),
   ),
